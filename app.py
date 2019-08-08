8f7c26748d9e5f50bfe590fba9a8f268ed0532df
Pomf <niveistellacide@gmail.com>
2019-08-08 00:45:33 -0400

    p80, index

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template("index.html")
+
+
+@app.route('/dtl')
+def dtl_shit():
+    return 'bruh'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=80)
